175697b2fd517443e99ce4d2e5242528fc4cb66b
Alexander Jiang <dashuai99@gmail.com>
2019-07-19 23:50:15 -0500

    Make the one row game script generalize into solving an MDP for any game size and outputting results (the value of each state, and comparing that value to the expected value if actions were picked uniformly at random)

--- one_row_game.py
+++ one_row_game.py
@@ -6,15 +6,19 @@ def print_state(state):
 
 def main():
     # BFS search to generate list of all possible states in the one-row game
-    num_cols = 4
+    num_rows = 2
+    num_cols = 2
     frontier = []
     visited = []
+    TWO_TILE_PROB = 1.0 # probability of spawning a 2-tile (vs a 4-tile) after a successful move
+    output_filename = f"mdp_{num_rows}x{num_cols}_prob{TWO_TILE_PROB}.txt"
 
     # initialize the possible starting states
-    for i in range(num_cols):
-        tiles = [[0] * num_cols]
-        tiles[0][i] = 1
-        frontier.append(GameState(nrows=1, ncols=num_cols, tiles=tiles, score=0, game_over=False))
+    for i in range(num_rows):
+        for j in range(num_cols):
+            tiles = [[0] * num_cols for r in range(num_rows)]
+            tiles[i][j] = 1
+            frontier.append(GameState(nrows=num_rows, ncols=num_cols, tiles=tiles, score=0, game_over=False))
 
     # perform BFS search to enumerate all states
     while len(frontier) > 0:
@@ -26,7 +30,7 @@ def main():
         visited.append(state)
 
         for move in state.moves_available():
-            successors = state.successor_states(move, prob_two_tile=1.0)
+            successors = state.successor_states(move, prob_two_tile=TWO_TILE_PROB)
             for probability, successor, reward in successors:
                 if successor in visited:
                     continue
@@ -37,73 +41,97 @@ def main():
     all_states = visited
     for state in all_states:
         print(state.tiles)
-    print(f"num visited: {len(all_states)}")
+    print(f"num states: {len(all_states)}")
+
 
     # value iteration on the MDP
+    with open(output_filename, "w") as output:
+        output.write(f"number of rows = {num_rows}\n")
+        output.write(f"number of columns = {num_cols}\n")
+        output.write(f"prob of 2-tile (vs. 4-tile) = {TWO_TILE_PROB}\n\n")
 
-    # initialize V and V_new to 0 for all states
-    V = {}
-    V_new = {}
-    for state in all_states:
-        state_str = str(state.tiles)
-        if state_str not in V:
-            V[state_str] = 0
-        V_new[state_str] = 0
+        output.write(f"number of states = {len(all_states)}\n")
 
-    converged = False
-    iter_num = 1
-    while not converged:
-        print(f"========== value iteration (iter # {iter_num})")
-        # update V_new using values in V
+        # initialize V and V_new to 0 for all states
+        V = {}
+        V_new = {}
         for state in all_states:
-            # print(state.tiles)
             state_str = str(state.tiles)
-            action_vals = {}
-            for move in state.moves_available():
-                # print(f" {move}")
-                successors = state.successor_states(move, prob_two_tile=1.0)
-                action_val = 0
-                for probability, successor, reward in successors:
-                    # print(f" -> {successor.tiles} (prob: {probability}, reward: {reward})")
-                    action_val += probability * (reward + V[str(successor.tiles)])
-                action_vals[move] = action_val
-                # print(f" {move} has value {action_val}")
+            if state_str not in V:
+                V[state_str] = 0
+            V_new[state_str] = 0
 
-            if not state.moves_available():
-                # print("found a terminal state")
-                continue
+        converged = False
+        iter_num = 1
+        while not converged:
+            print(f"========== value iteration (iter # {iter_num})")
+            # update V_new using values in V
+            for state in all_states:
+                # print(state.tiles)
+                state_str = str(state.tiles)
+                action_vals = {}
+                for move in state.moves_available():
+                    # print(f" {move}")
+                    successors = state.successor_states(move, prob_two_tile=TWO_TILE_PROB)
+                    action_val = 0
+                    for probability, successor, reward in successors:
+                        # print(f" -> {successor.tiles} (prob: {probability}, reward: {reward})")
+                        action_val += probability * (reward + V[str(successor.tiles)])
+                    action_vals[move] = action_val
+                    # print(f" {move} has value {action_val}")
 
-            # update V_new with the action with the highest value
-            best_action = None
-            best_action_val = float('-inf')
-            for action in action_vals:
-                if action_vals[action] > best_action_val:
-                    best_action = action
-                    best_action_val = action_vals[action]
+                if not state.moves_available():
+                    # print("found a terminal state")
+                    continue
 
-            if best_action_val > V[state_str]:
-                V_new[state_str] = best_action_val
-                print(f"V[{state_str}] updated from {V[state_str]} to {V_new[state_str]}")
+                # update V_new with the action with the highest value
+                best_action = None
+                best_action_val = float('-inf')
+                for action in action_vals:
+                    if action_vals[action] > best_action_val:
+                        best_action = action
+                        best_action_val = action_vals[action]
 
-        # convergence check: are any values in V and V_new "significantly different"?
-        converged = True
-        for state_str in V_new:
-            diff = V_new[state_str] - V[state_str]
-            if diff > 1e-5: # TODO arbitrary threshold...
-                converged = False
+                if best_action_val > V[state_str]:
+                    V_new[state_str] = best_action_val
+                    print(f"V[{state_str}] updated from {V[state_str]} to {V_new[state_str]}")
+
+            # convergence check: are any values in V and V_new "significantly different"?
+            converged = True
+            for state_str in V_new:
+                diff = V_new[state_str] - V[state_str]
+                if diff > 1e-5: # TODO arbitrary threshold...
+                    converged = False
+                    break
+
+            if converged:
+                print(f"Value iteration converged!")
                 break
 
-        if converged:
-            print(f"Value iteration converged!")
-            break
+            # if not converged, then copy from V_new to V and proceed to next iteration
+            for state_str in V_new:
+                V[state_str] = V_new[state_str]
 
-        # if not converged, then copy from V_new to V and proceed to next iteration
-        for state_str in V_new:
-            V[state_str] = V_new[state_str]
+            iter_num += 1
 
-        iter_num += 1
+        for state in all_states:
+            state_str = str(state.tiles)
+            output.write(f"state {state_str}: Value = {V[state_str]}\n")
 
+            action_total = 0
+            for move in state.moves_available():
+                successors = state.successor_states(move, prob_two_tile=TWO_TILE_PROB)
+                action_val = 0
+                for probability, successor, reward in successors:
+                    # output.write(f"    -> {successor.tiles} (prob: {probability}, reward: {reward})\n")
+                    action_val += probability * (reward + V[str(successor.tiles)])
+                output.write(f"    {move} has value {action_val}\n")
+                action_total += action_val
 
+            if len(state.moves_available()) > 0:
+                random_action_value = action_total / len(state.moves_available())
+                if random_action_value < V[state_str]:
+                    output.write(f"****Random move value = {random_action_value} (a loss of {V[state_str] - random_action_value})\n")
 
 
 if __name__ == "__main__": main()
